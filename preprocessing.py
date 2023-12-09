c62146e97783f1ff2d6a984691a12dd030abd72a
mafern <2martyf@gmail.com>
2023-12-09 16:31:05 -0700

    add comments and simplify preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -11,6 +11,7 @@ if machine == 'casper':
     root_dir = "/glade/campaign/cgd/cas/asphilli/ForceSMIP/"
 elif machine == 'asha':
     root_dir = "/barnes-scratch/DATA/ForceSMIP/"
+root_dir = '/Volumes/Data/Martin_Fernandez/ForceSMIP/'
     
 cmipTable = {
     "pr": "Amon",
@@ -48,129 +49,108 @@ fullmems = {
     "MIROC-ES2L": 30,
 }
 
-
 nlat = 72
 nlon = 144
 
-def load_model(model,var,timecut="Tier1",ntrainmems=20):
-    print(root_dir, cmipTable[var], var, model)
 
+def load_model(model, var, timecut="Tier1", ntrainmems=20):
+    print(root_dir, cmipTable[var], var, model)
+    # get the list of all files (members) for this model and variable
     filelist = root_dir + "Training/" + cmipTable[var] + "/" + var + "/" + model + "/" + var + "*.nc"
     filelist = glob.glob(filelist)
     filelist = sorted(filelist)
-
+    # time period to cut on
     timebds = evalPeriods[timecut]
 
+    # go through each file and load, append
     all_ens = []
-    for ifile,file in enumerate(filelist[:ntrainmems]):
+    for ifile, file in enumerate(filelist[:ntrainmems]):
         ds = xr.open_dataset(file)
+        # select the variable
         varin = ds[cmipVar[var]]
+        # get the correct time period
         varcut = varin.sel(time=slice(timebds[0],timebds[1]))
+        # make data yearly
+        #########################################################################
+        # currently always using yearly data
+        #########################################################################
         varcut = varcut.groupby('time.year').mean()  
-
-        varcut=varcut.assign_coords({"variant":ifile+1})
+        # add coordinate for concatenating after the loop
+        varcut = varcut.assign_coords({"variant":ifile+1})
+        # append into a list
         all_ens.append(varcut)
-
-    all_ens = xr.concat(all_ens,dim="variant")
+    # get all members into a single xarray data array
+    all_ens = xr.concat(all_ens, dim="variant")
     
     return all_ens
 
-def make_X_data(models = ["CESM2","MIROC6","CanESM5"], var = "tos", timecut = "Tier1", nmems = 20):
-    nmodels = len(models)
 
-    timebds = evalPeriods[timecut]
-    time1 = int(timebds[0][:4])
-    time2 = int(timebds[1][:4])
-
-    ntime = time2-time1+1
-
-    X = np.empty((ntime*nmems*nmodels,nlat,nlon))+np.nan
-
-    for imod, model in enumerate(models):
-
-        da=load_model(model,var,timecut,nmems)
-        da_np = np.asarray(da)
-        Xloop = np.empty((nmems*ntime,nlat,nlon))+np.nan
-
-        for imem in range(nmems):
-            Xloop[imem*ntime:(imem+1)*ntime,:,:] = da_np[imem,:,:,:]
-
-        X[nmems*ntime*imod:nmems*ntime*(imod+1),:,:] = Xloop
-
-    Xbad = np.mean(X,axis=0)
-    X[:,np.isnan(Xbad)] = 0
+def make_data(models=["CESM2", "MIROC6", "CanESM5"], var="tos", timecut="Tier1", mems=np.arange(20)):
+    """ 
+    models: a list of models from     "MIROC6
+                                       "CESM2"
+                                      "CanESM5"
+                                      "MPI-ESM1-2-LR"
+                                      "MIROC-ES2L"
     
-    return X
-
-
-def make_data(models = ["CESM2","MIROC6","CanESM5"], var = "tos", timecut = "Tier1", mems = np.arange(20)):
+    var: a variable string from     "pr"
+                                    "psl"
+                                    "tas"
+                                    "zmta"
+                                    "tos"
+                                    "siconc"
+                                    "monmaxpr":
+                                    "monmaxtasmax"
+                                    "monmintasmin"
     
-    # models: a list of models from     "MIROC6
-    #                                    "CESM2"
-    #                                   "CanESM5"
-    #                                   "MPI-ESM1-2-LR"
-    #                                   "MIROC-ES2L"
+    timecut: from the evaluation Tiers 
+        "Tier1" ("1950-01-01", "2022-12-31"),
+        "Tier2" ("1900-01-01", "2022-12-31"),
+        "Tier3" ("1979-01-01", "2022-12-31"),
     
-    # var: a variable string from     "pr"
-    #                                 "psl"
-    #                                 "tas"
-    #                                 "zmta"
-    #                                 "tos"
-    #                                 "siconc"
-    #                                 "monmaxpr":
-    #                                 "monmaxtasmax"
-    #                                 "monmintasmin"
-    
-    # timecut: from the evaluation Tiers 
-    #     "Tier1" ("1950-01-01", "2022-12-31"),
-    #     "Tier2" ("1900-01-01", "2022-12-31"),
-    #     "Tier3" ("1979-01-01", "2022-12-31"),
-    
-    # mems: a list of members to use
-    
-    nmodels = len(models)
-
-    timebds = evalPeriods[timecut]
-    time1 = int(timebds[0][:4])
-    time2 = int(timebds[1][:4])
-
-    ntime = time2-time1+1
-
+    mems: a list of members to use
+    """
+    # number of members requested --- why is mems an arange and not a number?
     nmems = len(mems)
-    
-    Yforced = np.empty((ntime*nmems*nmodels,nlat,nlon))+np.nan
-    Yinternal = np.empty((ntime*nmems*nmodels,nlat,nlon))+np.nan
-    Xfull = np.empty((ntime*nmems*nmodels,nlat,nlon))+np.nan
-    
+
+    # go through each model separately (targets are model specific)
     for imod, model in enumerate(models):
-
+        # get the number of members available for this model
         nfullmems = fullmems[model]
-        
-        da=load_model(model,var,timecut,nfullmems)
-        
+        # check that we can load the number of members requested
+        assert nmems <= nfullmems, f"more members requested ({nmems}) than available ({nfullmems})"
+        # load the members for this model/variable into an xarray
+        # returns the correct time period given the tier
+        da = load_model(model, var, timecut, nmems)
+        # convert from xarray to a numpy array: dimensions are [members, time, lat, lon]
         da_np = np.asarray(da)
-        da_f_np = np.mean(da_np,axis=0,keepdims=True)
-        da_i_np = da_np-da_f_np
-        
-        Yloop = np.empty((nmems*ntime,nlat,nlon))+np.nan
-        Yloop_i = np.empty((nmems*ntime,nlat,nlon))+np.nan
-        Yloop_f = np.empty((nmems*ntime,nlat,nlon))+np.nan
-        
-        for imem, mem in enumerate(mems):
-            Yloop_f[imem*ntime:(imem+1)*ntime] = da_f_np
-            Yloop_i[imem*ntime:(imem+1)*ntime] = da_i_np[mem,:,:,:]
-            Yloop[imem*ntime:(imem+1)*ntime] = da_np[mem,:,:,:]
-
-        Xfull[nmems*ntime*imod:nmems*ntime*(imod+1)] = Yloop
-        Yforced[nmems*ntime*imod:nmems*ntime*(imod+1)] = Yloop_f
-        Yinternal[nmems*ntime*imod:nmems*ntime*(imod+1)] = Yloop_i
+        # get the forced response as the ensemble mean across all members (0th dimension)
+        da_f_np = np.mean(da_np, axis=0, keepdims=True)
+        # internal variability is the difference between the full signal and the forced response
+        da_i_np = da_np - da_f_np
+        # reshape these to dimensions: [samples, lat, lon]
+        da_np = np.reshape(da_np, (-1, nlat, nlon))
+        # for forced response, repeat nmems times to match full and internal
+        da_f_np = np.reshape([da_f_np]*nmems, (-1, nlat, nlon))
+        da_i_np = np.reshape(da_i_np, (-1, nlat, nlon))
+        # stack the models together        
+        if imod == 0:
+            Xfull = da_np
+            Yforced = da_f_np
+            Yinternal = da_i_np
+        else:
+            Xfull = np.vstack([Xfull, da_np])
+            Yforced = np.vstack([Yforced, da_f_np])
+            Yinternal = np.vstack([Yinternal, da_i_np])
     
-    return Xfull,Yforced,Yinternal
+    return Xfull, Yforced, Yinternal
+
 
 def save_npz(settings, A_train, F_train, I_train, A_val, F_val, I_val):
     os.system('mkdir ' +  settings['npz_dir'])
-    np.savez(settings['npz_dir'] + settings['exp_name'] + '.npz', At = A_train, Ft = F_train, It = I_train, 
-             Av = A_val, Fv = F_val, Iv = I_val)
+    np.savez(settings['npz_dir'] + settings['exp_name'] + '.npz', At=A_train, Ft=F_train, It=I_train, 
+             Av=A_val, Fv=F_val, Iv=I_val)
+
 
 def load_npz(settings):
     npzdat = np.load(settings['npz_dir'] + settings['exp_name'] + '.npz')
@@ -179,12 +159,11 @@ def load_npz(settings):
     
 
 def stack_variable(X_tuple):
-    
-    Xout = np.stack(X_tuple,axis=-1)
-    
+    Xout = np.stack(X_tuple, axis=-1)
     return Xout
     
-def make_eval_mem(evalmem="1H",var="tos",timecut="Tier1"):
+
+def make_eval_mem(evalmem="1H", var="tos", timecut="Tier1"):
     
     filelist = root_dir + "Evaluation-"+timecut+"/" + cmipTable[var] + "/" + var + "/" + var + "*" + evalmem + "*.nc"
     file = glob.glob(filelist)[0]
@@ -195,13 +174,41 @@ def make_eval_mem(evalmem="1H",var="tos",timecut="Tier1"):
     
     return varmean_np
 
-def make_eval_mem_monthly(evalmem="1H",var="tos",timecut="Tier1"):
+
+def make_eval_mem_monthly(evalmem="1H", var="tos", timecut="Tier1"):
     
     filelist = root_dir + "Evaluation-"+timecut+"/" + cmipTable[var] + "/" + var + "/" + var + "*" + evalmem + "*.nc"
     file = glob.glob(filelist)[0]
     ds = xr.open_dataset(file)
     varin = ds[cmipVar[var]]
-    #varmean = varin.groupby("time.year").mean()
     varin_np = np.asarray(varin)
     
     return varin_np
+
+
+# def make_X_data(models=["CESM2","MIROC6","CanESM5"], var="tos", timecut="Tier1", nmems=20):
+#     nmodels = len(models)
+
+#     timebds = evalPeriods[timecut]
+#     time1 = int(timebds[0][:4])
+#     time2 = int(timebds[1][:4])
+
+#     ntime = time2 - time1 + 1
+
+#     X = np.empty((ntime*nmems*nmodels, nlat, nlon)) + np.nan
+
+#     for imod, model in enumerate(models):
+
+#         da = load_model(model, var, timecut, nmems)
+#         da_np = np.asarray(da)
+#         Xloop = np.empty((nmems*ntime, nlat, nlon)) + np.nan
+
+#         for imem in range(nmems):
+#             Xloop[imem*ntime:(imem+1)*ntime, :, :] = da_np[imem, :, :, :]
+
+#         X[nmems*ntime*imod:nmems*ntime*(imod+1), :, :] = Xloop
+
+#     Xbad = np.mean(X, axis=0)
+#     X[:, np.isnan(Xbad)] = 0
+    
+#     return X
