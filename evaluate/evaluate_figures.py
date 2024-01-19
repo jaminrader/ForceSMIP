3043a9266ee6817e428e74891479783a5fdf1164
mafern <2martyf@gmail.com>
2024-01-19 16:24:16 -0700

    fix naming, data/prediction loading, add member dimension

--- evaluate/evaluate_figures.py
+++ evaluate/evaluate_figures.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xarray as xr
+import matplotlib
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from cartopy.util import add_cyclic_point
@@ -16,12 +17,18 @@ import pandas as pd
 
 datapath = "exp_data/"
 predpath = "saved_predictions/"
-savepath = "..//figures/"
-savefig_name = "zmta_internal"
+savepath = "../figures/"
+savefig_name = "tos_internal"
 lat = np.linspace(-88.75, 88.75, 72)
 lon = np.linspace(1.25, 358.8, 144)
 lat_n = np.size(lat)
 lon_n = np.size(lon)
+
+lower = plt.cm.RdBu_r(np.linspace(0,.49, 49))
+white = plt.cm.RdBu_r(np.ones(2)*0.5)
+upper = plt.cm.RdBu_r(np.linspace(0.51, 1, 49))
+colors = np.vstack((lower, white, upper))
+tmap = matplotlib.colors.LinearSegmentedColormap.from_list('terrain_map_white', colors)
     
 # Atr = All training
 # Ftr = Forced training; truth for training
@@ -38,7 +45,7 @@ def load_npz(filename):
                                                         npzdat['Aev'])
     return Atr, Ftr, Itr, Ava, Fva, Iva, Ate, Fte, Ite, Aev
 
-Atr, Ftr, Itr, Ava, Fva, Iva, Ate, Fte, Ite, Aev = load_npz(datapath + "psl_exp_forced.npz")
+Atr, Ftr, Itr, Ava, Fva, Iva, Ate, Fte, Ite, Aev = load_npz(datapath + "tos_feature_internal.npz")
 Atr = np.nan_to_num(Atr)
 Ftr = np.nan_to_num(Ftr)
 Itr = np.nan_to_num(Itr)
@@ -49,7 +56,7 @@ time_n = np.size(time)
 # Pval = prediciton on valuation
 # Peval = predication on evaluation; truth not known.
 
-f = np.load(predpath + "psl_exp_forced0_preds.npz")
+f = np.load(predpath + "tos_feature_internal0_preds.npz")
 Ptrain, Pval, Peval, Ptest = f['Ptrain'], f['Pval'], f['Peval'], f['Ptest']
 Ptrain = np.nan_to_num(Ptrain)
 
@@ -57,8 +64,10 @@ Ptrain = np.nan_to_num(Ptrain)
 # set Truth variable and Prediction variable to the respective variable for the rest of the code to run
 Truth = Fte.squeeze().reshape(20, 73, 72, 144)
 Prediction = Ptest.reshape(20, 73, 72, 144)
+Full = (Ite+Fte).squeeze().reshape(20, 73, 72, 144)
 Truth = np.nan_to_num(Truth)
 Prediction = np.nan_to_num(Prediction)
+Full = np.nan_to_num(Full)
 
 Prediction = xr.DataArray(Prediction, dims = ['members','time','lat','lon'])
 Prediction["members"] = np.arange(20)
@@ -71,20 +80,26 @@ Truth["members"] = np.arange(20)
 Truth["time"] = time
 Truth["lat"] = lat[:]
 Truth["lon"] = lon[:]
+
+Full = xr.DataArray(Full, dims = ['members','time','lat','lon'])
+Full["members"] = np.arange(20)
+Full["time"] = time
+Full["lat"] = lat[:]
+Full["lon"] = lon[:]
 #######################################
 
 VARIABLE = "psl"
 TIER = 1
 
 sample_n = 1 #used for functions that plot a single sample
-PLOT_PRED_EXAMPLE = False
-PLOT_AVG_ABS_DIFF = False
+PLOT_PRED_EXAMPLE = True
+PLOT_AVG_ABS_DIFF = True
 
 PATTERN_CORRELATION = False
 
-SUB_YEAR_N_PERIOD = 73
+SUB_YEAR_N_PERIOD = 44
 SUB_PERIOD_TREND = False #"Sub-period trend (pattern correlation and RMSE)"
-FULL_PERIOD_TREND = False #"Full-period trend (pattern correlation and RMSE)"
+FULL_PERIOD_TREND = True #"Full-period trend (pattern correlation and RMSE)"
 
 #"Monthly and annual time series of global means and means in latitude bands (correlation and RMSE)"
 #"Grid point, monthly & annual time series (global-mean correlation and MSE)"
@@ -92,8 +107,8 @@ FULL_PERIOD_TREND = False #"Full-period trend (pattern correlation and RMSE)"
 
 EVALUATE_ENSO = False #SST
 EVALUATE_EW_PAC_SST = False #incomplete
-EVALUATE_NAO = True #SLP 
-EVALUATE_SAM = True #SLP
+EVALUATE_NAO = False #SLP 
+EVALUATE_SAM = False #SLP
 EVALUATE_SOI = False #incomplete
 EVALUATE_AA = False #incomplete
 
@@ -131,68 +146,80 @@ if EVALUATE_ENSO:
     plt.savefig(savepath + str(SAM) + "_" + savefig_name)
 
 if FULL_PERIOD_TREND:
-    Truth_Trend = np.empty(shape = (72,144))
-    Predicted_Trend = np.empty(shape = (72,144))
-    x_range = np.arange(0,time_n,1)
+    for member in range(20):
+        Pred = Prediction[member]
+        Tru = Truth[member]
+        Ful = Full[member]
 
-    for la in np.arange(0, lat_n, 1):
-        for lo in np.arange(0, lon_n, 1):
-            values = np.polyfit(x_range, Truth[:,la,lo], 1)
-            Truth_Trend[la,lo] = values[0] * time_n
-
-            values = np.polyfit(x_range, Prediction[:,la,lo], 1)
-            Predicted_Trend[la,lo] = values[0] * time_n
-
-    # Plot_Gobal_Map(Truth_Trend, "Spatial trend of Truth", -.2, .2, tmap, "")
-    # Plot_Gobal_Map(Predicted_Trend, "Spatial trend of Prediction", -.2, .2, tmap, "")
-    # Plot_Gobal_Map(Predicted_Trend-Truth_Trend, "Difference between truth and prediction", -.2, .2, tmap, "")
-    weighted_difference = (Predicted_Trend-Truth_Trend) * np.transpose(np.array([np.cos(np.deg2rad(lat)),]*144))
-    RMSE = np.sum((weighted_difference)**2/time_n)
-    PC = ef.CalcPatternCorrelation(Truth_Trend, Predicted_Trend)
-
-    axs = ef.Plot2b2_colormesh_o3(Truth_Trend, Predicted_Trend, Predicted_Trend-Truth_Trend, lat, lon, -.2, .2)
-    axs.text(430, 8, "The weighted RMSE is: " +str(round(RMSE, 2)), fontsize = 10)
-    axs.text(430, 30, "The weighted Pattern Correlation is: " +str(round(PC)), fontsize = 10)
-
-    plt.show()
-    plt.savefig(savepath + str(FullTrend) + "_" + savefig_name)
-
-if SUB_YEAR_N_PERIOD:
-    sub_time = np.arange(0, time_n, SUB_YEAR_N_PERIOD)
-    x_range_sub = np.arange(0,SUB_YEAR_N_PERIOD,1)
-
-    for s, sub in enumerate(sub_time[:-1]):
         Truth_Trend = np.empty(shape = (72,144))
         Predicted_Trend = np.empty(shape = (72,144))
+        Full_Trend = np.empty(shape = (72, 144))
+        x_range = np.arange(0,time_n,1)
+
         for la in np.arange(0, lat_n, 1):
             for lo in np.arange(0, lon_n, 1):
-                values = np.polyfit(x_range_sub, Truth[sub:sub_time[s+1],la,lo], 1)
-                Truth_Trend[la,lo] = values[0] * SUB_YEAR_N_PERIOD
+                values = np.polyfit(x_range, Tru[:,la,lo], 1)
+                Truth_Trend[la,lo] = values[0] * time_n
 
-                values = np.polyfit(x_range_sub, Prediction[sub:sub_time[s+1],la,lo], 1)
-                Predicted_Trend[la,lo] = values[0] * SUB_YEAR_N_PERIOD
+                values = np.polyfit(x_range, Pred[:,la,lo], 1)
+                Predicted_Trend[la,lo] = values[0] * time_n
 
-        # Plot_Gobal_Map(Predicted_Trend-Truth_Trend, "Difference between truth and prediction", -1, 1, tmap, "")
+                values = np.polyfit(x_range, Ful[:,la,lo], 1)
+                Full_Trend[la,lo] = values[0] * time_n
 
+        # Plot_Gobal_Map(Truth_Trend, "Spatial trend of Truth", -.2, .2, tmap, "")
+        # Plot_Gobal_Map(Predicted_Trend, "Spatial trend of Prediction", -.2, .2, tmap, "")
+        # Plot_Gobal_Map(Predicted_Trend-Truth_Trend, "Difference between truth and prediction", -.2, .2, tmap, "")
         weighted_difference = (Predicted_Trend-Truth_Trend) * np.transpose(np.array([np.cos(np.deg2rad(lat)),]*144))
-        RMSE = np.sum((weighted_difference)**2/SUB_YEAR_N_PERIOD)
-        PC = ef.CalcPatternCorrelation(Truth_Trend[:,:], Predicted_Trend[:,:]) 
+        RMSE = np.sum((weighted_difference)**2/time_n)
+        PC = ef.CalcPatternCorrelation(Truth_Trend, Predicted_Trend)
 
-        axs = ef.Plot2b2_colormesh_o3(Truth_Trend, Predicted_Trend, Predicted_Trend-Truth_Trend, lat, lon, -.2, .2)
-        axs.text(430, 8, "The weighted RMSE is: " +str(round(RMSE, 2)), fontsize = 10)
-        axs.text(430, 30, "The weighted Pattern Correlation is: " +str(round(PC)), fontsize = 10)
+        axs = ef.Plot2b2_colormesh_o3(Truth_Trend, Predicted_Trend, Predicted_Trend-Truth_Trend, Full_Trend, lat, lon, -2, 2)
+        # axs.text(430, 8, "The weighted RMSE is: " +str(round(RMSE, 2)), fontsize = 10)
+        # axs.text(430, 30, "The weighted Pattern Correlation is: " +str(round(PC)), fontsize = 10)
 
-        plt.show()
-        plt.savefig(savepath + str(FullTrend) + "_" + savefig_name)
+        plt.savefig(savepath + str(member) + "_Full_trend_" + savefig_name)
+
+if SUB_PERIOD_TREND:
+    for member in range(20):
+        Pred = Prediction[member]
+        Tru = Truth[member]
+
+        sub_time = np.arange(0, time_n, SUB_YEAR_N_PERIOD)
+        x_range_sub = np.arange(0,SUB_YEAR_N_PERIOD,1)
+
+        for s, sub in enumerate(sub_time[:-1]):
+            Truth_Trend = np.empty(shape = (72,144))
+            Predicted_Trend = np.empty(shape = (72,144))
+            for la in np.arange(0, lat_n, 1):
+                for lo in np.arange(0, lon_n, 1):
+                    values = np.polyfit(x_range_sub, Tru[sub:sub_time[s+1],la,lo], 1)
+                    Truth_Trend[la,lo] = values[0] * SUB_YEAR_N_PERIOD
+
+                    values = np.polyfit(x_range_sub, Pred[sub:sub_time[s+1],la,lo], 1)
+                    Predicted_Trend[la,lo] = values[0] * SUB_YEAR_N_PERIOD
+
+            # Plot_Gobal_Map(Predicted_Trend-Truth_Trend, "Difference between truth and prediction", -1, 1, tmap, "")
+
+            weighted_difference = (Predicted_Trend-Truth_Trend) * np.transpose(np.array([np.cos(np.deg2rad(lat)),]*144))
+            RMSE = np.sum((weighted_difference)**2/SUB_YEAR_N_PERIOD)
+            PC = ef.CalcPatternCorrelation(Truth_Trend[:,:], Predicted_Trend[:,:]) 
+
+            axs = ef.Plot2b2_colormesh_o3(Truth_Trend, Predicted_Trend, Predicted_Trend-Truth_Trend, lat, lon, -.2, .2)
+            axs.text(430, 8, "The weighted RMSE is: " +str(round(RMSE, 2)), fontsize = 10)
+            axs.text(430, 30, "The weighted Pattern Correlation is: " +str(round(PC)), fontsize = 10)
+
+            plt.savefig(savepath + str(member) + "_Sub_trend_" + savefig_name)
 
 if PLOT_PRED_EXAMPLE:
-    Plot_Gobal_Map(Prediction[0,:,:], "Example of a Prediction", None, None, "Reds", "")
-    plt.savefig(savepath + str(sample) + "_" + savefig_name)
+    ef.Plot_Gobal_Map(lat, lon, Prediction[sample_n,0,:,:], "Example of a Prediction", Prediction[sample_n,0,:,:].min(), Prediction[sample_n,0,:,:].max(), "Reds", "")
+    plt.savefig(savepath + "sample_" + savefig_name)
 
 if PLOT_AVG_ABS_DIFF:
-    Difference = np.mean(np.abs(Prediction - Truth), axis = 0)
+    Difference = np.mean(np.abs(Prediction[sample_n,:,:,:] - Truth[sample_n,:,:,:]), axis = 0)
     # Plot_Gobal_Map(plot_data, title, min, max, colorbar, colorbar_title)
-    Plot_Gobal_Map(Difference, "Average Absolute Difference between \nTruth and Prediction", 0, 1, "Reds", "")
-    plt.savefig(savepath + str(sample_dfference) + "_" + savefig_name)
+    ef.Plot_Gobal_Map(lat, lon, Difference, "Average Absolute Difference between \nTruth and Prediction", Difference.min(), Difference.max(), tmap, "")
+    plt.savefig(savepath + "sample_difference_" + savefig_name)
+    
 
 
