cad42292ee3162c376611317dce99142dece52f9
Jamin Rader <jaminkurtis@me.com>
2024-02-07 09:47:07 -0700

    added metrics

---
+++ metrics.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def MAE(Dtrue, Dpred, weights = None):
+    
+    metrics_arr = np.abs(Dtrue - Dpred)
+    nonanbool = ~np.isnan(metrics_arr)
+    if weights is not None:
+        weights = weights * np.ones_like(nonanbool)
+        weights = weights[nonanbool]
+        if len(weights) == 0:
+            weights = None
+    metrics_arr = metrics_arr[nonanbool]
+    return np.average(metrics_arr, weights=weights)
+
+
+def MSE(Dtrue, Dpred, weights = None):
+    metrics_arr = np.square(Dtrue - Dpred)
+    nonanbool = ~np.isnan(metrics_arr)
+    if weights is not None:
+        weights = weights * np.ones_like(nonanbool)
+        weights = weights[nonanbool]
+        if len(weights) == 0:
+            weights = None
+    metrics_arr = metrics_arr[nonanbool]
+    return np.average(metrics_arr, weights=weights)
+
+
+def R2(Dtrue, Dpred, weights=1):
+    if weights is None:
+        weights = 1
+    eps = np.finfo(float).eps
+    ss_res = np.nansum(np.square(Dtrue-Dpred)*weights) # sum of squares of the residual
+    ss_tot = np.nansum(np.square(Dtrue-np.mean(Dtrue))*weights) # total sum of squares
+    return ( 1 - ss_res/(ss_tot + eps) )
