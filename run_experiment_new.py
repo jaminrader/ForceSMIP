14e5d5d9dcbb3b73ebc402a09212ec027e211179
mafern <2martyf@gmail.com>
2024-02-26 12:51:16 -0700

    add true training and testing to train_and_predict function

--- run_experiment_new.py
+++ run_experiment_new.py
@@ -44,7 +44,8 @@ Xtrain_stand, Xval_stand, Xtest_stand, \
 # build model, train, save, make predictions
 PFtrain, PFval, PFtest, PItrain, PIval, PItest = \
     train_and_predict(Atrain, Aval, Atest,
-    ``                Xtrain_stand, Xval_stand, Xtest_stand,
+                      Ftrain, Itrain, Ftest, Itest,
+                      Xtrain_stand, Xval_stand, Xtest_stand,
                       Ttrain_stand, Tval_stand, Ttest_stand,
                       Ttrain_mean, Ttrain_std, 
                       Tval_mean, Tval_std, 
