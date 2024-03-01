e7832dbc07c6b062a30ddc1443efb4750fa99a38
Jamin Rader <jaminkurtis@me.com>
2024-03-01 14:20:16 -0700

    fixed target standardization?

--- standardize.py
+++ standardize.py
@@ -142,9 +142,12 @@ def unstandardize_predictions(Atrain, Aval, Atest,
     Ate = Atest[..., target_index]
 
     # unstandardize the predictions, in this case forced
-    Ptr = unstandardize_for_each_member(Ptr_stand, Ttr_mean, Ttr_std)
-    Pva = unstandardize_for_each_member(Pva_stand, Tva_mean, Tva_std)
-    Pte = unstandardize_for_each_member(Pte_stand, Tte_mean, Tte_std)
+    # Ptr = unstandardize_for_each_member(Ptr_stand, Ttr_mean, Ttr_std)
+    # Pva = unstandardize_for_each_member(Pva_stand, Tva_mean, Tva_std)
+    # Pte = unstandardize_for_each_member(Pte_stand, Tte_mean, Tte_std)
+    Ptr = unstandardize(Ptr_stand, Ttr_mean, Ttr_std)
+    Pva = unstandardize(Pva_stand, Tva_mean, Tva_std)
+    Pte = unstandardize(Pte_stand, Tte_mean, Tte_std)
     
     if settings['target_component'] == 'forced':
         # calculate internal as full - forced
@@ -190,38 +193,77 @@ def standardize_all_data(Atr, Ava, Ate,
     Xte_stand, __, __ = standardize_for_each_member(Xte_stand)
 
     # Standardize the outputs
-    Atr_stand, Atr_mean, Atr_std = standardize_for_each_member(Atr)
-    Ava_stand, Ava_mean, Ava_std  = standardize_for_each_member(Ava)
-    Ate_stand, Ate_mean, Ate_std  = standardize_for_each_member(Ate)
+    amean = Atr.mean(axis=(0))
+    astd = Atr.std(axis=(0))
+    Atr_stand = standardize(Atr, amean, astd)
+    Ava_stand = standardize(Ava, amean, astd)
+    Ate_stand = standardize(Ate, amean, astd)
 
-    Ftr_stand, Ftr_mean, Ftr_std = standardize_for_each_member(Ftr)
-    Fva_stand, Fva_mean, Fva_std  = standardize_for_each_member(Fva)
-    Fte_stand, Fte_mean, Fte_std  = standardize_for_each_member(Fte)
+    fmean = Ftr.mean(axis=(0))
+    fstd = Ftr.std(axis=(0))
+    Ftr_stand = standardize(Ftr, fmean, fstd)
+    Fva_stand = standardize(Fva, fmean, fstd)
+    Fte_stand = standardize(Fte, fmean, fstd)
 
-    Itr_stand, Itr_mean, Itr_std = standardize_for_each_member(Itr)
-    Iva_stand, Iva_mean, Iva_std  = standardize_for_each_member(Iva)
-    Ite_stand, Ite_mean, Ite_std  = standardize_for_each_member(Ite)
+    imean = Itr.mean(axis=(0))
+    istd = Itr.std(axis=(0))
+    Itr_stand = standardize(Itr, imean, istd)
+    Iva_stand = standardize(Iva, imean, istd)
+    Ite_stand = standardize(Ite, imean, istd)
 
     if settings['target_component'] == 'internal':
         Ttr_stand = Itr_stand
         Tva_stand = Iva_stand
         Tte_stand = Ite_stand
-        Ttr_mean = Itr_mean
-        Ttr_std = Itr_std
-        Tva_mean = Iva_mean
-        Tva_std = Iva_std
-        Tte_mean = Ite_mean
-        Tte_std = Ite_std
+        Ttr_mean = imean
+        Ttr_std = istd
+        Tva_mean = imean
+        Tva_std = istd
+        Tte_mean = imean
+        Tte_std = istd
     elif settings['target_component'] == 'forced':
         Ttr_stand = Ftr_stand
         Tva_stand = Fva_stand
         Tte_stand = Fte_stand
-        Ttr_mean = Ftr_mean
-        Ttr_std = Ftr_std
-        Tva_mean = Fva_mean
-        Tva_std = Fva_std
-        Tte_mean = Fte_mean
-        Tte_std = Fte_std
+        Ttr_mean = fmean
+        Ttr_std = fstd
+        Tva_mean = fmean
+        Tva_std = fstd
+        Tte_mean = fmean
+        Tte_std = fstd
+
+    # Atr_stand, Atr_mean, Atr_std = standardize_for_each_member(Atr)
+    # Ava_stand, Ava_mean, Ava_std  = standardize_for_each_member(Ava)
+    # Ate_stand, Ate_mean, Ate_std  = standardize_for_each_member(Ate)
+
+    # Ftr_stand, Ftr_mean, Ftr_std = standardize_for_each_member(Ftr)
+    # Fva_stand, Fva_mean, Fva_std  = standardize_for_each_member(Fva)
+    # Fte_stand, Fte_mean, Fte_std  = standardize_for_each_member(Fte)
+
+    # Itr_stand, Itr_mean, Itr_std = standardize_for_each_member(Itr)
+    # Iva_stand, Iva_mean, Iva_std  = standardize_for_each_member(Iva)
+    # Ite_stand, Ite_mean, Ite_std  = standardize_for_each_member(Ite)
+
+    # if settings['target_component'] == 'internal':
+    #     Ttr_stand = Itr_stand
+    #     Tva_stand = Iva_stand
+    #     Tte_stand = Ite_stand
+    #     Ttr_mean = Itr_mean
+    #     Ttr_std = Itr_std
+    #     Tva_mean = Iva_mean
+    #     Tva_std = Iva_std
+    #     Tte_mean = Ite_mean
+    #     Tte_std = Ite_std
+    # elif settings['target_component'] == 'forced':
+    #     Ttr_stand = Ftr_stand
+    #     Tva_stand = Fva_stand
+    #     Tte_stand = Fte_stand
+    #     Ttr_mean = Ftr_mean
+    #     Ttr_std = Ftr_std
+    #     Tva_mean = Fva_mean
+    #     Tva_std = Fva_std
+    #     Tte_mean = Fte_mean
+    #     Tte_std = Fte_std
 
     return  Xtr_stand, Xva_stand, Xte_stand, \
             Ttr_stand, Tva_stand, Tte_stand, \
