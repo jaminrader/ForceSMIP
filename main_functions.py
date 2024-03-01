fixed lat weighting hopefully

main_functions.py
@@ -136,7 +136,10 @@ def calculate_metrics(PFtrain, PFval, PFtest, PItrain, PIval, PItest, Ftrain, Fv
     results = {}
 
     # make the weights
-    lats = np.linspace(-90, 90, PFtrain.shape[1]+1)
+    if settings_dict["target_variable"] != "zmta":
+        lats = np.linspace(-90, 90, PFtrain.shape[1]+1)
+    else:
+        lats = np.linspace(-90, 90, PFtrain.shape[2]+1)
     lats = [(ll+lh)/2 for ll, lh in zip(lats[:-1], lats[1:])]
     weights = np.cos(np.deg2rad(lats))
     if settings_dict["target_variable"] != "zmta":