cb5a429570f54c06b0adf54030574b2153e94037
mafern <2martyf@gmail.com>
2025-07-23 13:39:08 -0600

    fix a few problems, change save names

--- CreateSubmission.py
+++ CreateSubmission.py
@@ -23,12 +23,6 @@ experiment_name = ARGS.exp_name
 variable = ARGS.var # tos, tas, pr, psl, zmta, monmaxtasmax, monmintasmin, monmaxpr, mrso
 Tier = ARGS.tier
 # -------------------------------------------------------------------------------------------
-if variable == "zmta":
-    IS_Zonal_avg = True
-    shape = [plev_n, lat_n]
-else:
-    IS_Zonal_avg = False
-    shape = [lat_n, lon_n]
 if Tier == "T1":
     date = pd.date_range(start='1/1/1950', end='1/1/2023', freq='M')
     memberID = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
@@ -47,11 +41,16 @@ lon_n = np.size(lon)
 plev = np.linspace(1000, 200, 17)
 plev_n = np.size(plev)
 months = ["10", "20", "30", "40", "50", "60", "70", "80", "90", "100", "110", "120"]
+if variable == "zmta":
+    IS_Zonal_avg = True
+    shape = [plev_n, lat_n]
+else:
+    IS_Zonal_avg = False
+    shape = [lat_n, lon_n]
 
 DirectoryPath = "./"
 loadDirectoryPath = DirectoryPath + "saved_predictions/"
 saveDirectoryPath = DirectoryPath + "SubmissionFiles/"
-figureDirectoryPath = DirectoryPath + "evaluate/figures/"
 
 CreateSaveFile = True
 PlotForcedTrend = True
@@ -63,7 +62,7 @@ CheckSavedFile = True
 predictions = np.empty(shape=(12, nyears*np.size(memberID), shape[0], shape[1], 1))
 # for i, month in enumerate(months):
 for i, month in enumerate(months):
-    predictionFile = experiment_name + variable + "_" + month + "_preds.npz"
+    predictionFile = experiment_name + '_' + variable + "_" + month + "_preds.npz"
     f = np.load(loadDirectoryPath + predictionFile)
     PFtest = f['PFtest'] #(years*evaluation_members, lat, lon, 1)
     
@@ -113,8 +112,6 @@ for i, member in enumerate(memberID):
         if IS_Zonal_avg:
             save_data = xr.DataArray(final_data[i, :, :, :], dims = ['time', 'plev', 'lat'],
                                 coords = [date, plev, lat])
-            print(type(save_data))
-            print(save_data)
             save_data.name = variable
 
             exists = os.path.isfile(saveDirectoryPath + saveFilename + ".nc")
@@ -125,7 +122,6 @@ for i, member in enumerate(memberID):
         else:
             save_data = xr.DataArray(final_data[i, :, :, :], dims=['time', 'lat', 'lon'], coords=[date, lat, lon])
             save_data.name = variable
-            print(save_data)
             
             exists = os.path.isfile(saveDirectoryPath + saveFilename + ".nc")
             if exists:
@@ -150,7 +146,7 @@ for i, member in enumerate(memberID):
             plt.plot(date, globalmean, label=memberID[i])
             plt.legend()
             plt.title("Predicted Forced Response for " + variable + " " + memberID[i])
-            plt.savefig(DirectoryPath + "evaluate/figures/predictedForcedResponses" + memberID[i] + ".png")
+            plt.savefig(DirectoryPath + "evaluate/figures/predictedForcedResponses" + memberID[i] + f"_{variable}{Tier}.png")
         else:
             globalmean = xr.DataArray(final_data[i, :, :, :], dims = ['time', 'lat', 'lon'])
             globalmean["time"] = date
@@ -163,7 +159,7 @@ for i, member in enumerate(memberID):
             plt.plot(date, globalmean, label=memberID[i])
             plt.legend()
             plt.title("Predicted Forced Response for " + variable + " " + memberID[i])
-            plt.savefig(DirectoryPath + "evaluate/figures/predictedForcedResponses" + memberID[i] + ".png")
+            plt.savefig(DirectoryPath + "evaluate/figures/predictedForcedResponses" + memberID[i] + f"_{variable}{Tier}.png")
 
     if PlotExample:
         if IS_Zonal_avg:
@@ -171,7 +167,7 @@ for i, member in enumerate(memberID):
             cs = plt.pcolormesh(plev, lat, final_data[0, sample_n, :, :], cmap="Reds")
             cbar = plt.colorbar(cs, shrink=0.7, orientation='horizontal', label='Surface Air Temperature (C)', format='%.1f')#, pad=5)
             plt.title("Sample " + str(sample_n))
-            plt.savefig(DirectoryPath + "evaluate/figures/sample" + memberID[i] + ".png")
+            plt.savefig(DirectoryPath + "evaluate/figures/sample" + memberID[i] + f"_{variable}{Tier}.png")
         else:
             plt.figure(dpi=(200), figsize=(6, 4))
             ax=plt.axes(projection= ccrs.PlateCarree())
@@ -181,24 +177,24 @@ for i, member in enumerate(memberID):
             cbar = plt.colorbar(cs, shrink=0.7, orientation='horizontal',label='Surface Air Temperature (C)', format='%.1f')#, pad=5)
             # cbar.set_label(colorbar_title)
             plt.title("Sample " + str(sample_n))
-            plt.savefig(DirectoryPath + "evaluate/figures/sample" + memberID[i] + ".png")
+            plt.savefig(DirectoryPath + "evaluate/figures/sample" + memberID[i] + f"_{variable}{Tier}.png")
 
     if CheckSavedFile:
         if IS_Zonal_avg:
             f = xr.open_dataset(saveDirectoryPath + saveFilename + ".nc")
-            plotdata = f["forced"]
+            plotdata = f[variable]
             
             plt.figure(dpi=(200), figsize=(6, 4))
             cs = plt.pcolormesh(plotdata[0, :, :], cmap="Reds")
             cbar = plt.colorbar(cs)
             plt.title(variable + " " + memberID[i])
-            plt.savefig(DirectoryPath + "evaluate/figures/OPEN" + memberID[i] + ".png")
+            plt.savefig(DirectoryPath + "evaluate/figures/OPEN" + memberID[i] + f"_{variable}{Tier}.png")
         else:
             f = xr.open_dataset(saveDirectoryPath + saveFilename + ".nc")
-            plotdata = f["forced"]
+            plotdata = f[variable]
             
             plt.figure(dpi=(200), figsize=(6, 4))
             cs = plt.pcolormesh(lon, lat, plotdata[0, :, :], cmap ="Reds")
             cbar = plt.colorbar(cs)
             plt.title(variable + " " + memberID[i])
-            plt.savefig(DirectoryPath + "evaluate/figures/OPEN" + memberID[i] + ".png")
+            plt.savefig(DirectoryPath + "evaluate/figures/OPEN" + memberID[i] + f"_{variable}{Tier}.png")
